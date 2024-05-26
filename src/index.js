eaa7cd4f77fdee9d0ec0aed66b660110e5826b09
Your Name <your.email@example.com>
2024-05-26 13:21:32 +0530

    Remove react-hover-image and clean up dependencies

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
 import store from './redux-state/CartState';
-
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   
+    <ChakraProvider>
 
       <Provider store={store}>
         <App />
       </Provider>
 
-    
+    </ChakraProvider>
   </React.StrictMode>
 );
 
